4b6026d484376de98581a166e02b40d2bf4a659f
fazfaz7 <adrian.faz123@gmail.com>
2023-05-03 17:16:36 -0600

    First commit

---
+++ EnsenaFinalAppStore/EnsenaFinalAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnsenaFinalAppStoreApp.swift
+//  EnsenaFinalAppStore
+//
+//  Created by Adrian Faz on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct EnsenaFinalAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
